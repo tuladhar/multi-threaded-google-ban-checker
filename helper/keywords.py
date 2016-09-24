266479ccffbc9f7086bb256b89913d09449fe0c6
Puru Tuladhar <tuladharpuru@gmail.com>
2016-09-24 17:06:42 +0545

    Update to use keywords.txt file

--- helper/keywords.py
+++ helper/keywords.py
@@ -1,7 +1,7 @@
 import random
 
 # TODO: load from file
-with open('helper/ipcheck_keywords.txt', 'r') as f:
+with open('helper/keywords.txt', 'r') as f:
 	keywords = [keyword.strip() for keyword in f if keyword.strip()]
 
 def get_random_keyword():
